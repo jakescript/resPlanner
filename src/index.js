95ad2ef85701caff95e69af82307495e9e2e6874
sjsamphex <sjsamphex@gmail.com>
2020-11-20 15:41:41 -0800

    fixed reservation viewer

--- src/index.js
+++ src/index.js
@@ -1,39 +1,59 @@
-import axios from "axios";
+import axios from 'axios';
 
-const userList = document.querySelector("#users");
-const restList = document.querySelector("#resturants");
-const reservList = document.querySelector("#reservations");
+const userList = document.querySelector('#users');
+const restList = document.querySelector('#resturants');
+const reservList = document.querySelector('#reservations');
+let users = [];
+let resturants = [];
+let resList = [];
 
-const userView = (users) => `${users.map(u => `<li> <a href="#${u.id}">${u.name}</a></li>`).join("")}`;
-const resturantView = (resturants) => `${resturants.map(rest => `<li>${rest.name}</li>`).join("")}`;
+const userView = (users) =>
+  `${users.map((u) => `<li> <a href="#${u.id}">${u.name}</a></li>`).join('')}`;
+const resturantView = (resturants) =>
+  `${resturants.map((rest) => `<li>${rest.name}</li>`).join('')}`;
 
-const reservationView = (reservations) => `
-${reservations.map(r => {
-  const rest =
+function reservationView(reservations) {
+  if (reservations.length) {
+    const html = reservations
+      .map(
+        (reservation) => `
+      <li data-id='${reservation.id}'>
+        ${reservation.id}
+        <br />
+        (
+        ${new Date(reservation.createdAt).toLocaleDateString()}
+        ${new Date(reservation.createdAt).toLocaleTimeString()}
+        )
+      </li>
+    `
+      )
+      .join('');
+    return html;
+  } else {
+    return '<li>no reservations</li>';
+  }
+}
 
-}).join("")}`;
-
-
-const init = async() => {
+const init = async () => {
   try {
-    const [users, resturants] = await Promise.all([
-      axios.get("/api/users"),
-      axios.get("/api/restaurants")
+    [users, resturants] = await Promise.all([
+      axios.get('/api/users'),
+      axios.get('/api/restaurants'),
     ]);
     // const users = (await axios.get("/api/users")).data;
     userList.innerHTML = userView(users.data);
     restList.innerHTML = resturantView(resturants.data);
-    // reservList.innerHTML = reservationView(users);
+    reservList.innerHTML = reservationView(resList.data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 init();
 
-window.addEventListener("hashchange", async(e) => {
-  const id = window.location.hash.slice(1) * 1
-  const resList = await axios.get(`/api/users/${id}/reservations`)
-
-  reservList.innerHTML = reservationView(resList.data);
-})
+window.addEventListener('hashchange', async (e) => {
+  const id = window.location.hash.slice(1) * 1;
+  resList = await axios.get(`/api/users/${id}/reservations`);
+  init();
+  // reservList.innerHTML = reservationView(resList.data);
+});
